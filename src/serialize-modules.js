8146c50d92aae55eb2518cfa9497acba2071fa07
David Aurelio <dev@david-aurelio.com>
2013-03-27 22:29:54 +0100

    Switch to closure variables to make parameter lists shorter

--- src/serialize-modules.js
+++ src/serialize-modules.js
@@ -13,7 +13,7 @@ exports.serializeModules = serializeModules;
 function serializeModules(entryPointNames, resolve, callback) {
   'use strict';
 
-  loadModules([], {}, Q.denodeify(resolve), entryPointNames, []).nodeify(callback);
+  load(Q.denodeify(resolve), entryPointNames).nodeify(callback);
 }
 
 function startsWith(haystack, needle) {
@@ -24,62 +24,69 @@ function id(o) {
   return o;
 }
 
-function loadModules(modules, modulePromises, resolve, names, requestedBy) {
-  'use strict';
+function load(resolve, entryPointNames) {
+  var modules = [], modulePromises = {};
 
-  return Q.all(names.map(function(name) {
-    return loadModule(modules, modulePromises, resolve, name, requestedBy);
-  })).spread(id);
-}
+  function loadModules(names, requestedBy) {
+    'use strict';
 
-function addModule(module, modules) {
-  'use strict';
-
-  modules.push(module);
-  return modules;
-}
-
-function addDependenciesAndModule(modules, modulePromises, resolve, module, requestedBy) {
-  'use strict';
-
-  requestedBy = requestedBy.concat(module.name);
-  return loadModules(modules, modulePromises, resolve, module.dependencies, requestedBy)
-    .then(addModule.bind(null, module));
-}
-
-function moduleLoaded(modules, modulePromises, resolve, module, requestedBy) {
-  'use strict';
-
-  var dependencies = module.dependencies;
-  if (dependencies) {
-    dependencies = dependencies.filter(function(dependencyName) {
-      return requestedBy.indexOf(dependencyName) === -1;
-    });
-  }
-  return dependencies && dependencies.length ?
-    addDependenciesAndModule(modules, modulePromises, resolve, module, requestedBy) :
-    addModule(module, modules);
-}
-
-function modulePromise(modules, modulePromises, resolve, name, requestedBy) {
-  'use strict';
-
-  return resolve(name)
-    .then(function(module) {
-      return moduleLoaded(modules, modulePromises, resolve, module, requestedBy);
-    });
-}
-function loadModule(modules, modulePromises, resolve, name, requestedBy) {
-  'use strict';
-
-  if (startsWith(name, '../') || startsWith(name, './')) {
-    return Q.reject(Error('Relative module name passed'));
+    return Q.all(names.map(function(name) {
+      return loadModule(name, requestedBy);
+    })).spread(id);
   }
 
-  if (!modulePromises.hasOwnProperty(name)) {
-    modulePromises[name] =
-      modulePromise(modules, modulePromises, resolve, name, requestedBy);
+  function addModule(module) {
+    'use strict';
+
+    modules.push(module);
+    return modules;
   }
 
-  return modulePromises[name];
+  function addDependenciesAndModule(module, requestedBy) {
+    'use strict';
+
+    requestedBy = requestedBy.concat(module.name);
+    return loadModules(module.dependencies, requestedBy)
+      .then(addModule.bind(null, module));
+  }
+
+  function moduleLoaded(module, requestedBy) {
+    'use strict';
+
+    var dependencies = module.dependencies;
+    if (dependencies) {
+      dependencies = dependencies.filter(function(dependencyName) {
+        return requestedBy.indexOf(dependencyName) === -1;
+      });
+    }
+    return dependencies && dependencies.length ?
+      addDependenciesAndModule(module, requestedBy) :
+      addModule(module);
+  }
+
+  function modulePromise(name, requestedBy) {
+    'use strict';
+
+    return resolve(name)
+      .then(function(module) {
+        return moduleLoaded(module, requestedBy);
+      });
+  }
+
+  function loadModule(name, requestedBy) {
+    'use strict';
+
+    if (startsWith(name, '../') || startsWith(name, './')) {
+      return Q.reject(Error('Relative module name passed'));
+    }
+
+    if (!modulePromises.hasOwnProperty(name)) {
+      modulePromises[name] =
+        modulePromise(name, requestedBy);
+    }
+
+    return modulePromises[name];
+  }
+
+  return loadModules(entryPointNames, []);
 }
