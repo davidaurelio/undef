9357e11347e7492d5b0348e72a2cfa00f16491c5
David Aurelio <dev@david-aurelio.com>
2013-03-29 11:45:55 +0100

    Add simple parse function using esprima

---
+++ src/parse-esprima.js
@@ -0,0 +1,13 @@
+var esprima = require('esprima');
+
+exports.parse = function(source, callback) {
+  process.nextTick(function() {
+    var result;
+    try {
+      result = esprima.parse(source);
+    } catch (error) {
+      callback(error);
+    }
+    callback(null, result);
+  });
+};
