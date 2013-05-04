f5e719fc4b5086e7034fc008aa380fd761e01ea8
David Aurelio <dev@david-aurelio.com>
2013-05-04 15:38:45 +0200

    Reorganize code structure

--- test/serialize-test.js
+++ test/serialize-test.js
@@ -2,7 +2,7 @@ var buster = require('buster');
 var format = require('util').format;
 
 require('./assertions').addAll(buster.assertions);
-var serializeModules = require('../src/serialize-modules').serializeModules;
+var serializeModules = require('../src/serialize').serializeModules;
 var assert = buster.assert, refute = buster.refute;
 
 function moduleToString() {
